1c3f8ca9e5496191dab34b730e3bfc77d483a09b
chen <chenzimmer2@gmail.com>
2022-03-07 19:14:02 +0200

    Fix db

--- server/mongoose/DatabaseEndpoint.ts
+++ server/mongoose/DatabaseEndpoint.ts
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
+const { config } = require('dotenv'); 
 
 export const connectToDatabase = async () => {
-    require('dotenv').config();
-    console.log(process.env.MONGO_URI);
-    // Connecting to the database
     mongoose
       .connect(process.env.MONGO_URI || 'mongodb+srv://admin:Aa123456@teamtable.mii7y.mongodb.net/TeamTable', {
         useNewUrlParser: true,
