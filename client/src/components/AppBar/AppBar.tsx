2defb4f2d1055400757ad6b12f0c1421a8869545
noam_barda <noam404@gmail.com>
2022-03-10 19:37:20 +0200

    created landing page

--- client/src/components/AppBar/AppBar.tsx
+++ client/src/components/AppBar/AppBar.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import Menu from '@mui/material/Menu';
-import { useAuth } from '../../auth/AuthProvider';
+import {useAuth} from '../../auth/AuthProvider';
 import MenuItem from '@mui/material/MenuItem';
 import Box from '@mui/material/Box';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+// @ts-ignore
+import logo from '../../../assets/images/TeamTableLogo.png'
 
 
 export default function MenuAppBar() {
@@ -28,29 +28,24 @@ export default function MenuAppBar() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" style={{ background: 'white' }}>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            TeamTable
-          </Typography>
+          <Box sx={{ flexGrow: 1 }}>
+              <Box
+                  component="img"
+                  sx={{
+                    height: 64,
+                  }}
+                  alt="logo"
+                  src={logo}/>
+              </Box>
           <div>
             <IconButton
               size="large"
               aria-label="account of current user"
               aria-controls="menu-appbar"
               aria-haspopup="true"
-              onClick={handleMenu}
-              color="inherit"
-            >
+              onClick={handleMenu}>
               <AccountCircle />
             </IconButton>
             <Menu
--- client/src/components/screens/home/Home.tsx
+++ client/src/components/screens/home/Home.tsx
@@ -1,20 +1,42 @@
 import React from "react";
-import { Navigate, Outlet, useLocation } from "react-router-dom";
-import {  useAuth } from "../../../auth/AuthProvider";
+import {useLocation} from "react-router-dom";
+import {useAuth} from "../../../auth/AuthProvider";
+// @ts-ignore
+import TeamTableTheme from '../../../../assets/images/TeamTableTheme.jpg';
+import {Box, Button, Container, Paper, Typography} from "@mui/material";
+import {ArrowForward} from "@mui/icons-material";
 
 const Home: React.FC = (props): JSX.Element => {
-  const auth = useAuth();
-  const location = useLocation();
+    const auth = useAuth();
+    const location = useLocation();
 
-  // if (!auth.user) {
-  //   return <Navigate to="/login-screen" state={{ from: location }} replace />;
-  // }
-
-  return (
-    <div>
-        Home Page
-    </div>
-  );
+    // if (!auth.user) {
+    //   return <Navigate to="/login-screen" state={{ from: location }} replace />;
+    // }
+    return (
+        <Box sx={{flexGrow: 1}}>
+            <Container maxWidth="lg">
+                <Paper sx={{backgroundImage: `url(${TeamTableTheme})`, height: 450}}>
+                    <Box sx={{display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "end",
+                        width: "fit-content",
+                        marginBottom: 15,
+                        marginLeft: 10,
+                        height: "-webkit-fill-available"}}>
+                    <Typography sx={{backgroundColor: "white", marginBottom: 1, paddingX: 3}}>
+                        Let us find you the perfect restaurant
+                    </Typography>
+                    <Button variant="contained"
+                            endIcon={<ArrowForward />}
+                            sx={{backgroundColor: '#3ED3D6'}}>
+                        Find us a table
+                    </Button>
+                    </Box>
+                </Paper>
+            </Container>
+        </Box>
+    );
 };
 
 export default Home;
