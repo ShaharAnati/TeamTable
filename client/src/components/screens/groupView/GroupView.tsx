ef503ac1e4ef41c066e75171721d53260101402e
barda <noam404@gmail.com>
2022-05-07 19:26:13 +0300

    added group link and option to copy to clipboard

--- client/src/components/screens/groupView/GroupView.tsx
+++ client/src/components/screens/groupView/GroupView.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import {Button} from "@mui/material";
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 const GroupView: React.FC = (): JSX.Element => {
-    
     return (
-        <p>GroupView Page</p>
+        <Button variant="outlined"
+                size="medium"
+                color="inherit"
+                endIcon={<ContentCopyIcon/>}
+                onClick={() => navigator.clipboard.writeText(window.location.href)}>
+            {window.location.href}
+        </Button>
     );
 }
 
