1ceba1a7d55474a00c56b76e4a1556296fb1e277
asaf-dev <sooki0974@gmail.com>
2022-05-10 22:41:38 +0300

    working date and time filtering

--- client/src/components/screens/findRestaurants/FindRestaurans.tsx
+++ client/src/components/screens/findRestaurants/FindRestaurans.tsx
@@ -1,22 +1,35 @@
+import dayjs from "dayjs";
 import Grid from "@mui/material/Grid";
 import Chip from "@mui/material/Chip";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
 import React, { useState } from "react";
 import { AllRestaurants } from "../restaurants/allRestaurants";
-import Switch from '@mui/material/Switch';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-import { TimePicker } from '@mui/x-date-pickers/TimePicker';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Switch from "@mui/material/Switch";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import TagFilters from "./TagFilters";
+import "./FindRestaurans.css";
 
-const tags = ["meat", "vegan", "good"];
+const tags = [
+  "meat",
+  "vegan",
+  "good",
+  "dsfsd",
+  "gdfsf",
+  "dfhfgfdgdf",
+  "sdfgsdg",
+  "fsdfdsfs",
+  "dsfdsf",
+];
 
 type TagCetegory = "common" | "cuisine" | "kashrut";
 
@@ -26,31 +39,50 @@ type Tag = {
   label: string;
 };
 
-export const FindRestaurants = (props): JSX.Element => {
-  const [chosedTags, setChosedTags] = useState([]);
-  const [value, setValue] = React.useState<Date | null>(
-    new Date('2018-01-01T00:00:00.000Z'),
-  );
-  const [day, setDay] = React.useState('');
+type Filters = {
+  timeRange?: string[2];
+  day?: string;
+  hour?: string;
+  tags?: any[];
+};
 
-  const handleTagsChange = (event, value) => {
-    setChosedTags(value);
+export const FindRestaurants = (props): JSX.Element => {
+  const [filters, setFilters] = useState<Filters>({});
+
+  const handleTagsChange = (value) => {
+    setFilters({
+      ...filters,
+      tags: value,
+    });
   };
 
-  const handleDayChange = (event: SelectChangeEvent) => {
-    setDay(event.target.value as string);
+  const handleDayChange = (event: SelectChangeEvent, value) => {
+    setFilters({
+      ...filters,
+      day: event.target.value,
+    });
+  };
+
+  const handleHourChange = (value) => {
+    setFilters({
+      ...filters,
+      hour: dayjs(value).format("HH:mm"),
+    });
   };
 
   return (
     <Grid container spacing={2}>
       <Grid item xs={2}>
-        <div style={
-          {
-            height: ' 100px',
-            border: 'black',
-            borderWidth: '1px',
-            borderStyle: 'dashed'
-          }} >Group Details</div>
+        <div
+          style={{
+            height: "100px",
+            border: "black",
+            borderWidth: "1px",
+            borderStyle: "dashed",
+          }}
+        >
+          Group Details
+        </div>
         <div>
           <FormControl component="fieldset">
             <FormGroup aria-label="position" row>
@@ -74,51 +106,43 @@ export const FindRestaurants = (props): JSX.Element => {
               />
             </FormGroup>
           </FormControl>
-          <Autocomplete
-            multiple
-            id="tags-outlined"
-            options={tags}
-            getOptionLabel={(option) => option}
+          <TagFilters
+            tags={tags}
+            selectedTags={filters.tags}
             onChange={handleTagsChange}
-            filterSelectedOptions
-            renderInput={(params) => (
-              <TextField {...params} variant="standard" label="Filters" />
-            )}
           />
         </div>
-        <LocalizationProvider dateAdapter={AdapterDateFns} style={{
-          display: 'flex',
-          flexDirection: 'row'
-        }} >
+        <LocalizationProvider
+          dateAdapter={AdapterDayjs}
+          style={{
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
           <TimePicker
-            value={value}
-            onChange={setValue}
-            renderInput={(params) => <TextField {...params} />}
-          /> -
-          <TimePicker
-            value={value}
-            onChange={setValue}
+            label="Hour"
+            value={filters.hour ? dayjs(`01-01-2000 ${filters.hour}`) : null}
+            onChange={handleHourChange}
             renderInput={(params) => <TextField {...params} />}
           />
-
           <FormControl fullWidth>
             <InputLabel id="demo-simple-select-label">Day</InputLabel>
             <Select
               labelId="demo-simple-select-label"
               id="demo-simple-select"
-              value={day}
+              value={filters.day || ""}
               label="Day"
               onChange={handleDayChange}
             >
-              {[1, 2, 3, 4, 5, 6, 7].map(day => (
+              {[1,   2, 3, 4, 5, 6, 7].map((day) => (
                 <MenuItem value={day}>{day}</MenuItem>
               ))}
             </Select>
           </FormControl>
-        </LocalizationProvider >
+        </LocalizationProvider>
       </Grid>
       <Grid item xs={10}>
-        <AllRestaurants chosedTags={chosedTags} day={day} />
+        <AllRestaurants filters={filters} />
       </Grid>
     </Grid>
   );
--- client/src/components/screens/restaurants/allRestaurants.tsx
+++ client/src/components/screens/restaurants/allRestaurants.tsx
@@ -11,9 +11,15 @@ import {
 import Chip from "@mui/material/Chip";
 import _ from "lodash";
 import FavoriteIcon from "@mui/icons-material/Favorite";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import restaurants from "./restaurants.json";
+import dayjs from "dayjs";
+import isSameOrAfter from "dayjs/plugin/isSameOrAfter";
+import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
 import "./restaurant.css";
+
+dayjs.extend(isSameOrAfter);
+dayjs.extend(isSameOrBefore);
 
 const styles = {
   header: {
@@ -26,16 +32,41 @@ const styles = {
 };
 
 type Props = {
+  restaurant: any;
   onFavoriteClick?: Function;
-  name: string;
-  description: string;
-  tags: string[];
   chosedTags: string[];
-  imgUrl: string;
+};
+
+const OpeningHours = ({ openingTimes }): JSX.Element => {
+  const dayMapping = {
+    "1": "א",
+    "2": "ב",
+    "3": "ג",
+    "4": "ד",
+    "5": "ה",
+    "6": "ו",
+    "7": "ש",
+  };
+
+  return (
+    <div>
+      {Object.keys(dayMapping).map((day) => (
+        <div className="openingHours-day-row" key={day}>
+          <div className="openingHours-day-label">{dayMapping[day]}</div>
+          <div className="openingHours-day-times">
+            {openingTimes[day]
+              ? `${openingTimes[day][0]} - ${openingTimes[day][1]}`
+              : "סגור"}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 const Restaurant = (props: Props): JSX.Element => {
-  const { name, description, imgUrl, tags, chosedTags } = props;
+  const { restaurant, chosedTags = [] } = props;
+  const { name, description, tags, imgUrl, openingTimes } = restaurant;
 
   return (
     <Card sx={{ maxWidth: 345 }}>
@@ -66,13 +97,49 @@ const Restaurant = (props: Props): JSX.Element => {
               : {})}
           />
         ))}
+        <OpeningHours openingTimes={openingTimes} />
       </CardContent>
     </Card>
   );
 };
 
 export const AllRestaurants = (props): JSX.Element => {
-  const { chosedTags, day } = props;
+  const { chosedTags, day, filters } = props;
+
+  const [filteredRestaurants, setFilteredRestaurants] = useState(restaurants);
+
+  useEffect(() => {
+    const filterByTags = (restaurant) =>
+      !filters.tags ||
+      filters.tags?.length === 0 ||
+      _.intersection(filters.tags, restaurant.tags).length > 0;
+
+    const filterByDay = (restaurant) =>
+      !filters.day || restaurant.openingTimes[filters.day] != null;
+
+    const filterByHour = (restaurant) => {
+      const { day, hour } = filters;
+      const wasNotChosen = !day || !hour;
+      if (wasNotChosen) return true;
+
+      if (!restaurant.openingTimes[day]) return false;
+
+      const openingHour = restaurant.openingTimes[day][0];
+      const closingHour = restaurant.openingTimes[day][1];
+
+      return (
+        dayjs(`01-01-2000 ${hour}`).isSameOrAfter(
+          `01-01-2000 ${openingHour}`
+        ) &&
+        dayjs(`01-01-2000 ${hour}`).isSameOrBefore(`01-01-2000 ${closingHour}`)
+      );
+    };
+
+    setFilteredRestaurants(
+      restaurants.filter(filterByTags).filter(filterByDay).filter(filterByHour)
+    );
+  }, [filters]);
+
   return (
     <div
       style={{
@@ -81,25 +148,11 @@ export const AllRestaurants = (props): JSX.Element => {
         justifyContent: "flex-start",
       }}
     >
-      {restaurants
-        .filter(
-          (rest) =>
-            chosedTags.length === 0 ||
-            _.intersection(chosedTags, rest.tags).length > 0
-        ).filter(rest => (
-          rest.openingTimes[day] != null
-        ))
-        .map((restaurant, i) => (
-          <div key={i} style={{ margin: "16px" }}>
-            <Restaurant
-              name={restaurant.name}
-              description={restaurant.description}
-              imgUrl={restaurant.imgeUrl}
-              tags={restaurant.tags}
-              chosedTags={chosedTags}
-            />
-          </div>
-        ))}
+      {filteredRestaurants.map((restaurant, i) => (
+        <div key={i} style={{ margin: "16px" }}>
+          <Restaurant restaurant={restaurant} chosedTags={filters.tags} />
+        </div>
+      ))}
     </div>
   );
 
