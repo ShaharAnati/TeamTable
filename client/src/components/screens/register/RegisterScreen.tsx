f2dccc8d5de966c87ebfeb82ac237c1f5803ff77
Shahar Anati <shahar.anati@gmail.com>
2022-05-16 00:30:40 +0300

    fix validations

--- client/src/components/screens/register/RegisterScreen.tsx
+++ client/src/components/screens/register/RegisterScreen.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { Navigate, useLocation, useNavigate } from 'react-router-dom';
-import { Button, Grid, IconButton, InputAdornment, makeStyles, Paper, TextField, Tooltip, Typography } from "@mui/material";
+import { Button, Grid, InputAdornment, makeStyles, Paper, TextField, Tooltip, Typography } from "@mui/material";
 import { LocationState, useAuth } from '../../../auth/AuthProvider';
 import './Login.css'
 import React from 'react';
@@ -12,16 +12,17 @@ const Register = (): JSX.Element => {
 
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
-    const [backgroundColor, setBackgroundColor] = useState<string>('');
-
-    const [name, setName] = useState<string>('');
+    const [fullName, setName] = useState<string>('');
     const [phoneNumber, setPhoneNumber] = useState<string>('');
-    const [isUsernameInvalid, setIsUsernameInvalid] = useState<boolean>(false);
-    const [isPasswordInvalid, setIsPasswordInvalid] = useState<boolean>(false);
-    const [isPhoneNumberInvalid, setIsPhoneNumberInvalid] = useState<boolean>(false);
-    const [isNameInvalid, setIsNameInvalid] = useState<boolean>(false);
+
+    const [usernameErrorText, setUsernameErrorText] = useState<string>('');
+    const [passwordErrorText, setPasswordErrorText] = useState<string>('');
+    const [phoneNumberErrorText, setPhoneNumberErrorText] = useState<string>('')
+    const [fullnameErrorText, setFullnameErrorText] = useState<string>('');
     const [doesUserAlreadyExist, setDoesUserAlreadyExist] = useState<boolean>(false);
-    const [errorMessage, setErrorMessage] = useState('')
+
+    const [areAllFieldsValid, setAreAllFieldsValid] = useState<boolean>(false);
+    const REQUIRED_FIELDS = [username, password, fullName, phoneNumber];
     
     const auth = useAuth();
     const navigate = useNavigate();
@@ -31,83 +32,91 @@ const Register = (): JSX.Element => {
         return <Navigate to="/" state={{ from: location }} replace />;
     }
 
-    const validate = (): void => {
-        usernameChanged(username)
-        passwordChanged(password)
-        nameChanged(name)
-        phoneNumberChanged(phoneNumber)
-        
-        if (!isUsernameInvalid && !isNameInvalid && !isPasswordInvalid && !isPhoneNumberInvalid) {
-            handleSubmit();
+    useEffect(() => {
+        setAreAllFieldsValid(
+            REQUIRED_FIELDS.every(field => field.length > 0) && 
+            !!!usernameErrorText && 
+            !!!passwordErrorText && 
+            !!!fullnameErrorText && 
+            !!!phoneNumberErrorText
+        )
+    }, [
+        username,
+        fullName,
+        phoneNumber,
+        password,
+        usernameErrorText,
+        passwordErrorText,
+        fullnameErrorText,
+        phoneNumberErrorText
+    ])
+
+    useEffect(() => {
+        if (doesUserAlreadyExist) {
+            setUsernameErrorText('Username is taken!')
         }
-    };
-
-    function checkIfPhoneNumberValid(input_str: string) {
-        var regex = /^[0]\d{2}\d{7}$/;
-
-        return regex.test(input_str)
-    }
-
-    function checkIfFullNameValid(input_str: string) {
-        var regex = /^[a-zA-Z]+\s{1}[a-zA-Z]+$/;
-
-        return regex.test(input_str) 
-    }
-
-    function checkIfusernameValid(input_str: string) {
-        var regex = /^\S+$/;
-
-        return regex.test(input_str) 
-    }
-
-    function usernameChanged(username) {
-        checkIfusernameValid(username) ? setIsUsernameInvalid(false) : setIsUsernameInvalid(true);
-        setDoesUserAlreadyExist(false);
-    }
-
-    function phoneNumberChanged(phoneNumber) {
-        checkIfPhoneNumberValid(phoneNumber) ? setIsPhoneNumberInvalid(false) : setIsPhoneNumberInvalid(true);
-    }
-
-    function checkIfpasswordValid(input_str: string) {
-        if (input_str == "") { 
-            return false
-        } else if (validator.isStrongPassword(input_str, {
-            minLength: 8, minLowercase: 1,
-            minUppercase: 1, minNumbers: 1, minSymbols: 1
-        })) {
-            setBackgroundColor("#0F9D58");
-            return true
-        } else {
-            setBackgroundColor("#DB4437");
-            return false
-        }
-    }
-
-    function passwordChanged(password) {
-        checkIfpasswordValid(password) ? setIsPasswordInvalid(false) : setIsPasswordInvalid(true);
-    }
+    }, [doesUserAlreadyExist])
     
-    function nameChanged(name) {
-        checkIfFullNameValid(name) ? setIsNameInvalid(false): setIsNameInvalid(true);
+    const validateUsername = (username: string): void => {
+        setDoesUserAlreadyExist(false);
+        const regex = /^\S+$/;
+        if (!username) {
+            setUsernameErrorText('Field is required')
+        } else  {
+            setUsernameErrorText(regex.test(username) ? '' : 'Username cannot have spaces in it')
+        }
+    }
+
+    const validateFullName = (fullname: string): void => {
+        const regex = /^[a-zA-Z]+\s{1}[a-zA-Z]+$/;
+        if (!fullname) {
+            setFullnameErrorText('Field is required')
+        } else {
+            setFullnameErrorText(regex.test(fullname) ? '' : 'Full name should contain only first and last names') 
+        }
+
+    }
+
+    const validatePhoneNumber = (phoneNumber: string): void => {
+        const regex = /^[0]\d{2}\d{7}$/;
+
+        if (!phoneNumber) {
+            setPhoneNumberErrorText('Field is required');
+        } else {
+            setPhoneNumberErrorText(regex.test(phoneNumber) ? '' : 'Please enter a valid 10 digit phone number')
+        }
+    }
+
+    const validatePassword = (password: string): void => {
+        if (!password) { 
+            setPasswordErrorText('Field is required');
+        } else if (validator.isStrongPassword(password, {
+            minLength: 8, 
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 1
+        })) {
+            setPasswordErrorText('Password too weak, see requirements');
+        } else {
+            setPasswordErrorText('');
+        }
     }
 
     const handleSubmit = async () => {
-        if (!isUsernameInvalid && !isPasswordInvalid) {
-            // proceed to submit
-            try {
-                await auth.register(username, password, phoneNumber, name);
+        try {
+            await auth.register(username, password, phoneNumber, fullName);
 
-                const state = location.state as LocationState;
-                const from = state?.from?.pathname || "/";
+            const state = location.state as LocationState;
+            const from = state?.from?.pathname || "/";
 
-                navigate(from, { replace: true })
-            } catch (err) {
-                if (err instanceof ConflictError) {
-                    setDoesUserAlreadyExist(true);
-                }
+            navigate(from, { replace: true })
+        } catch (err) {
+            if (err instanceof ConflictError) {
+                setDoesUserAlreadyExist(true);
             }
         }
+        
     };
 
     return (
@@ -124,30 +133,36 @@ const Register = (): JSX.Element => {
                             variant="standard"
                             value={username}
                             onChange={(e) => setUsername(e.target.value)}
-                            error={isUsernameInvalid || doesUserAlreadyExist }
-                            helperText={(isUsernameInvalid && "Username should be one word") || (doesUserAlreadyExist && "This username already exists")}
+                            onBlur={() => validateUsername(username)}
+                            error={!!usernameErrorText}
+                            helperText={usernameErrorText}
                             style={{height:'80px' }}
-                            fullWidth required />
+                            fullWidth required 
+                        />
                         <TextField label='Full Name'
                             name="fullname"
                             placeholder='Enter full name'
                             variant="standard"
-                            value={name}
+                            value={fullName}
                             onChange={(e) => setName(e.target.value)}
-                            error={isNameInvalid}
-                            helperText={(isNameInvalid && "Full name should contains first & last name with letters")}
+                            onBlur={() => validateFullName(fullName)}
+                            error={!!fullnameErrorText}
+                            helperText={(fullnameErrorText)}
                             style={{height:'80px' }}
-                            fullWidth required />
+                            fullWidth required 
+                        />
                         <TextField label='Phone Number'
                             name="phone"
                             placeholder='Enter phone number'
                             variant="standard"
                             value={phoneNumber}
                             onChange={(e) => setPhoneNumber(e.target.value)}
-                            error={isPhoneNumberInvalid }
-                            helperText={(isPhoneNumberInvalid && "Phone number should contains 10 digits")}
+                            onBlur={() => validatePhoneNumber(phoneNumber)}
+                            error={!!phoneNumberErrorText }
+                            helperText={phoneNumberErrorText}
                             style={{height:'80px' }}
-                            fullWidth />
+                            fullWidth 
+                        />
                         <TextField label='Password'
                             name="password"
                             placeholder='Enter password'
@@ -155,13 +170,13 @@ const Register = (): JSX.Element => {
                             type='password'
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
-                            error={isPasswordInvalid}
-                            helperText={isPasswordInvalid && "The password is invalid! Please read the requierments"}
+                            onBlur={() => validatePassword(password)}
+                            error={!!passwordErrorText}
+                            helperText={passwordErrorText}
                             fullWidth required 
                             style={{height:'80px' }}
                             InputProps={{
-                                startAdornment: (
-                                    
+                                startAdornment: ( 
                                   <InputAdornment position="start">
                                     <Tooltip 
                                     aria-multiline={true} 
@@ -174,17 +189,27 @@ const Register = (): JSX.Element => {
                                         <Typography color="inherit">* a minimum of 1 numeric character [0-9] </Typography>
                                         <Typography color="inherit">* a minimum of 1 special character </Typography>
                                         </>
-                                    }>
+                                    }>  
                                         <InfoIcon />
                                     </Tooltip>
                                   </InputAdornment>
-                                ),
+                                )
                               }}/>
                     </ div>
 
 
                     <div style={{ display: 'flex', justifyContent: 'center'}}>
-                        <Button type='submit' color='primary' variant="contained" className={'button-style'} style={{ width: '50%' }} onClick={validate}>Sign up</Button>
+                        <Button 
+                            type='submit'
+                            color='primary'
+                            variant="contained"
+                            className={'button-style'}
+                            style={{ width: '50%' }}
+                            disabled={!areAllFieldsValid}
+                            onClick={() => handleSubmit()}
+                        >
+                            Sign up
+                        </Button>
                     </div>
                 </Paper>
             </Grid>
