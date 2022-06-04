c1b8e6a180f7fd7264141d55c3a54b6421944741
asaf-dev <sooki0974@gmail.com>
2022-06-04 11:48:05 +0300

    added likedRestaurants to model and routes

--- server/mongoose/UserSchema.ts
+++ server/mongoose/UserSchema.ts
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
-import { Model } from 'mongoose';
+import { Model } from "mongoose";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, unique: true },
   password: { type: String },
-  phoneNumber: {type: String },
-  fullName: {type: String},
-  tokens: {type:[String]}
+  phoneNumber: { type: String },
+  fullName: { type: String },
+  tokens: { type: [String] },
+  likedRestaurants: { type: [String] },
 });
 
 const schema = mongoose.model("users", userSchema);
---
+++ server/routers/UsersRouter.ts
@@ -0,0 +1,50 @@
+import { Router } from "express";
+import { Document } from "mongoose";
+import { TagGroup } from "../models/Restaurant";
+
+import UserSchema from "../mongoose/UserSchema";
+
+const buildRouter = (): Router => {
+  const router: Router = Router();
+
+  router.patch("/like-restaurant", async (req, res) => {
+    try {
+      const { email, restaurant_id } = req.body;
+
+      const user = await UserSchema.findOne({ email });
+
+      if (!user.likedRestaurants.find((r: string) => r === restaurant_id)) {
+        const newlikedRestaurants = [...user.likedRestaurants, restaurant_id];
+        await UserSchema.findOneAndUpdate(
+          { email },
+          { likedRestaurants: newlikedRestaurants }
+        );
+      }
+    } catch (error) {
+      res.status(500).json([]);
+    }
+  });
+
+  router.delete("/unlike-restaurant", async (req, res) => {
+    try {
+      const { email, restaurant_id } = req.body;
+
+      const user = await UserSchema.findOne({ email });
+      const newlikedRestaurants = user.likedRestaurants.filter(
+        (r: string) => r === restaurant_id
+      );
+      await UserSchema.findOneAndUpdate(
+        { email },
+        { likedRestaurants: newlikedRestaurants }
+      );
+
+      res.status(200);
+    } catch (error) {
+      res.status(200).json([]);
+    }
+  });
+
+  return router;
+};
+
+export default buildRouter;
