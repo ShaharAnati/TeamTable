d3ce1cdefd7ed810a24c09c74fc938d8b6464c14
Shahar Anati <shahar.anati@gmail.com>
2022-06-11 17:09:20 +0300

    feat(homepage): create slide show

--- client/src/components/screens/AlternativeHome/AltHome.tsx
+++ client/src/components/screens/AlternativeHome/AltHome.tsx
@@ -1,15 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
-// @ts-ignore
-import BackgroundImg from "../../../../assets/images/background.jpg";
+import { ArrowForward } from "@mui/icons-material";
+import { Button} from "@mui/material";
+
 import { Restaurant } from "../../../../../server/models/Restaurant";
 import { AllRestaurants } from "../findRestaurants/restaurants/allRestaurants";
-import { Box, Button, Container, Paper, Typography } from "@mui/material";
-import { ArrowForward } from "@mui/icons-material";
 
 import "./AltHome.css";
-import axios from "axios";
 
 type Props = {};
 
@@ -22,34 +21,40 @@ function AltHome({}: Props) {
     axios(`/restaurants`).then(rests => {
       setRestaurantsToShow(rests.data);
     })
-}, [])
+  }, [])
+
+  const homePageHeaderContent: JSX.Element = (
+    <>
+      <div className="image-text">Where are we eating today?</div>
+      <Button
+        variant="contained"
+        endIcon={<ArrowForward />}
+        component={Link}
+        to="/create-group-screen"
+        sx={{ backgroundColor: "#00000080", color: "white" }}
+      >
+        Find us a table
+      </Button>
+
+      <div className="add-restaurant-text">
+        <span className="own-restaurant">Didn't find a restaurant?</span>
+        <span
+          className="add-now-button"
+          onClick={() => navigate("/create-restaurant", { replace: true })}
+        >
+          Add it <u>here</u> now!
+        </span>
+      </div>
+    </>
+  )
+
 
   return (
     <div>
       <div
         className="background-img"
-        style={{ backgroundImage: `url(${BackgroundImg})` }}
       >
-        <div className="image-text">Where do we eat today?</div>
-        <Button
-          variant="contained"
-          endIcon={<ArrowForward />}
-          component={Link}
-          to="/create-group-screen"
-          sx={{ backgroundColor: "#00000080", color: "white" }}
-        >
-          Find us a table
-        </Button>
-
-        <div className="add-restaurant-text">
-          <span className="own-restaurant">Didn't find a restaurant?</span>
-          <span
-            className="add-now-button"
-            onClick={() => navigate("/create-restaurant", { replace: true })}
-          >
-            Add it <u>here</u> now!
-          </span>
-        </div>
+        {homePageHeaderContent}
       </div>
 
       <div className="all-restaurant-area">
